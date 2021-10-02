9496b74be145e93942083afa6ff44367d6888b9f
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2021-10-01 17:51:25 -0700

    A beautiful flashy background

---
+++ src/ts/game/game.ts
@@ -0,0 +1,23 @@
+export class Game {
+
+    canvas!: HTMLCanvasElement;
+    context!: CanvasRenderingContext2D;
+
+    totalDt: number = 0;
+
+    constructor() {
+
+    }
+
+
+    update(dt: number): void {
+        this.totalDt += dt;
+    }
+
+    render(context: CanvasRenderingContext2D): void {
+        const h = 360 * this.totalDt;
+        context.fillStyle = `hsl(${h.toFixed(2)}, 50%, 50%)`;
+        context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+    }
+
+}
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,7 +1,35 @@
+import { Game } from "./game/game";
+
+const timeStep = 1 / 60;
+
+let canvas: HTMLCanvasElement;
+let context: CanvasRenderingContext2D;
+let game: Game;
+
 function init(): void {
-    const elem = document.createElement('div');
-    elem.innerText = 'hoi'
-    document.body.append(elem);
+    canvas = document.querySelector('.canvas') as HTMLCanvasElement;
+    context = canvas.getContext('2d')!;
+
+    game = new Game();
+
+    requestAnimationFrame(doAnimationFrame);
 }
+
+function doAnimationFrame(): void {
+    // TODO: The fixed time step stuff.
+    fixedUpdate();
+    render();
+
+    requestAnimationFrame(doAnimationFrame);
+}
+
+function fixedUpdate() {
+    game.update(timeStep);
+}
+
+function render() {
+    game.render(context);
+}
+
 
 window.onload = init;
